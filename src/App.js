028f7bb7cbcea8318921ab8b6865147fcc7834bc
Marcus Elmer <marcuselmer@sbcglobal.net>
2019-03-05 05:54:08 -0600

    Update footer, add react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import Header from './header.js';
+import Header from './header.js'; //import global header.
+//import Footer from './footer.js'; remove global footer per instructions.
+import hotDog from './assets/tab_lunch@2x.png';
+import internets from './assets/tab_internets@2x.png'
 
 class App extends Component {
 
@@ -35,9 +38,22 @@ class App extends Component {
     return (
       <div>
         <Header/>
-          {this.state.food.map((item) => 
-            <img src={item.backgroundImageURL} alt="" key={item.name} className="col-xs-12 col-sm-6 col-md-6 col-lg-6 food-image"/>
-          )}
+          <div className="background">
+            {this.state.food.map((item) => 
+              <img src={item.backgroundImageURL} alt="" key={item.name} className="col-xs-12 col-sm-6 col-md-6 col-lg-6 food-image"/>
+            )}
+          </div>
+          {/*removing global footer and only rendering on initial component.*/}
+          <div className="footer-btn col-xs-12 col-sm-12 col-md-12 col-lg-12">
+              <button type="" className="footer-btn col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <img src={hotDog} alt="" className="footer-img" />
+                  <p className="header-text">Lunch</p>
+              </button>
+              <button type="" className="footer-btn col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <img src={internets} alt="" className="footer-img" />
+                  <p className="header-text">Internets</p>
+              </button>
+          </div>
       </div>
     );
   }
